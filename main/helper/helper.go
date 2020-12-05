a78c4603834b58b15398426288df6def1a3030c2
raphael groemmer <r.groemmer@yahoo.de>
2020-12-05 23:42:29 +0100

    day4

---
+++ main/helper/helper.go
@@ -0,0 +1,32 @@
+package helper
+
+import (
+	"bufio"
+	"io"
+	"strconv"
+)
+
+func ReadInts(r io.Reader) ([]int, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	var result []int
+	for scanner.Scan() {
+		x, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return result, err
+		}
+		result = append(result, x)
+	}
+	return result, scanner.Err()
+}
+
+func ReadeFile(r io.Reader) []string {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+	var result []string
+	for scanner.Scan() {
+		x := scanner.Text()
+		result = append(result, x)
+	}
+	return result
+}
